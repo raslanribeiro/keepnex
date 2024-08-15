73eed51ddcb63cacd98972b5594a593ef2e12b0f
raslanribeiro <raslan.ribeiro@cortex-intelligence.com>
2024-08-15 19:10:28 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Tálita Floriano</h1>
+            <div className="button-container">
+                <a href="https://wa.me/5521969133563" target="_blank" rel="noopener noreferrer">
+                    <button>WhatsApp</button>
+                </a>
+                <a href="https://www.linkedin.com/in/talitafloriano/" target="_blank" rel="noopener noreferrer">
+                    <button>LinkedIn</button>
+                </a>
+                <a href="https://www.instagram.com/talitafloriano" target="_blank" rel="noopener noreferrer">
+                    <button>Instagram</button>
+                </a>
+                <a href="mailto:talitafloriano@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <button>Gmail</button>
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
