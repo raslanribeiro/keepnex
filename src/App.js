608e6ffe4ff397ea9dccc4153fc75073e47038f6
raslanribeiro <raslan.ribeiro@cortex-intelligence.com>
2024-08-16 20:01:32 -0300

    Chnaged default home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import VCard from "vcard-creator";
 // const fs = require("fs");
 import photo from "./assets/talita.jpeg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
     const handleDownloadContact = () => {
@@ -60,24 +61,33 @@ function App() {
     };
 
     return (
-        <div className="App">
-            <h1>Tálita Floriano</h1>
-            <div className="button-container">
-                <a href="https://wa.me/5521969133563" target="_blank" rel="noopener noreferrer">
-                    <button>WhatsApp</button>
-                </a>
-                <a href="https://www.linkedin.com/in/talitafloriano" target="_blank" rel="noopener noreferrer">
-                    <button>LinkedIn</button>
-                </a>
-                <a href="https://www.instagram.com/talitafloriano" target="_blank" rel="noopener noreferrer">
-                    <button>Instagram</button>
-                </a>
-                <a href="mailto:talitafloriano@gmail.com" target="_blank" rel="noopener noreferrer">
-                    <button>Gmail</button>
-                </a>
-                <button onClick={handleDownloadContact}>Add Contact</button>
-            </div>
-        </div>
+        <Router>
+            <Routes>
+                <Route
+                    path="/a0e8d2fd993f1f0600d5105ba7db2dc5"
+                    element={
+                        <div className="App">
+                            <h1>Tálita Floriano</h1>
+                            <div className="button-container">
+                                <a href="https://wa.me/5521969133563" target="_blank" rel="noopener noreferrer">
+                                    <button>WhatsApp</button>
+                                </a>
+                                <a href="https://www.linkedin.com/in/talitafloriano" target="_blank" rel="noopener noreferrer">
+                                    <button>LinkedIn</button>
+                                </a>
+                                <a href="https://www.instagram.com/talitafloriano" target="_blank" rel="noopener noreferrer">
+                                    <button>Instagram</button>
+                                </a>
+                                <a href="mailto:talitafloriano@gmail.com" target="_blank" rel="noopener noreferrer">
+                                    <button>Gmail</button>
+                                </a>
+                                <button onClick={handleDownloadContact}>Add Contact</button>
+                            </div>
+                        </div>
+                    }
+                />
+            </Routes>
+        </Router>
     );
 }
 
